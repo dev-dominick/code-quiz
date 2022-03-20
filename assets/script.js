32a7fa88ee583a88632926816486a00d5b5f3fbf
Dom <Domalbano35@gmail.com>
2022-03-20 19:27:47 -0400

    added header, a greeting, and a start button

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,38 @@
-console.log("hello world");
+
+
+
+
+
+
+
+// GIVEN I am taking a code quiz
+// WHEN I click the start button
+// THEN a timer starts and I am presented with a question
+
+
+
+//make start button
+//hook button up to begin timer
+//set interval
+
+// WHEN I answer a question
+// THEN I am presented with another question
+
+//on click to to pass to next question
+
+
+// WHEN I answer a question incorrectly
+// THEN time is subtracted from the clock
+
+//time penalty for wrong answer
+
+
+
+// WHEN all questions are answered or the timer reaches 0
+// THEN the game is over
+
+//condition for when timer = 0 to end the game
+
+
+// WHEN the game is over
+// THEN I can save my initials and my score
